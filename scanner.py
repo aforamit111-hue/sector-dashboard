0475596a9642f0ea8a729287a01761d127c4b7f7
aforamit111-hue <aforamit111@gmail.com>
2026-06-21 11:11:51 -0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -221,11 +221,6 @@ for stock in stocks:
         )
 
 # -------------------------
-# TEST
-# -------------------------
-stocks[0]["debug"] = "HELLO_AMIT"
-
-# -------------------------
 # Save JSON
 # -------------------------
 with open("stocks.json", "w", encoding="utf-8") as f:
